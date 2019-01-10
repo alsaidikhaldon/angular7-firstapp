7ec86eb5932bee1a3e7cec6041d0ee04a7243154
alsaidikhaldon <alsaidi.khaldon@gmail.com>
2019-01-11 00:45:19 +0100

    component cration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { AgencyComponent } from './agency/agency.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent,
+    AgencyComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
